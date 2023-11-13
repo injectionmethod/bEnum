67cb547577a7e00637929512a5fc022d5612c392
injectionmethod <80434330+injectionmethod@users.noreply.github.com>
2023-11-13 16:52:32 +0000

    Added source code

---
+++ benum/benum/Module1.vb
@@ -0,0 +1,49 @@
+﻿Imports System.Net.Http
+Imports Microsoft.VisualBasic.ApplicationServices
+Module Module1
+    Sub Main()
+        GC.Collect()
+        If Environment.GetCommandLineArgs(1).Contains("@") Then
+            bNum(Environment.GetCommandLineArgs(1))
+        Else
+            If IO.File.Exists(Environment.GetCommandLineArgs(2)) Then
+                For Each site In IO.File.ReadLines(Environment.GetCommandLineArgs(2))
+                    Console.WriteLine($"Using domain: {site}")
+                    For Each user In IO.File.ReadAllLines(Environment.GetCommandLineArgs(1))
+                        bNum(user + "@" + site)
+                    Next
+                Next
+            Else
+                For Each user In IO.File.ReadAllLines(Environment.GetCommandLineArgs(1))
+                    bNum(user + "@" + Environment.GetCommandLineArgs(2))
+                Next
+            End If
+        End If
+    End Sub
+    Function bNum(payload As String)
+        Dim url As String = "https://mail.google.com/mail/gxlu?email=" + payload
+
+        Using httpClient As New HttpClient(New HttpClientHandler() With {
+            .AllowAutoRedirect = False ' Prevent automatic redirection           
+        })
+            Try
+                Dim response As HttpResponseMessage = httpClient.GetAsync(url).Result
+
+                If response.StatusCode = System.Net.HttpStatusCode.NoContent Then
+                    ' Check for the "Set-Cookie" header in the response headers
+                    For Each header In response.Headers
+                        If header.Key = "Set-Cookie" Then
+                            Console.WriteLine($"User Found: {url}")
+                        End If
+                    Next
+                Else
+                    Console.WriteLine($"Received a {response.StatusCode} response.")
+                End If
+
+            Catch e As HttpRequestException
+                Console.WriteLine($"An error occurred: {e.Message}")
+            End Try
+        End Using
+        Return Nothing
+    End Function
+End Module
---
+++ benum/benum/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("benum")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("benum")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8cdc4ce4-b5e8-4cca-a7d0-da44c18605a0")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
